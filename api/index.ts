4b57031869c99e366d1b46a8280206302240cda2
Your Name <your.email@example.com>
2026-07-26 02:37:14 +0530

    Fix serverless function paths for Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../packages/backend/src/app.module';
+import { AppModule } from './_backend/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
